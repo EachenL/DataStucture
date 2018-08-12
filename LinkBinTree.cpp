f5352ff1f9ab0a352695daf92152ca30c6479f62
markning <shundyning@gmail.com>
2018-08-12 21:59:04 +0800

    add PreCreLBT

--- LinkBinTree.cpp
+++ LinkBinTree.cpp
@@ -2,8 +2,38 @@
 // Created by markning on 8/10/18.
 //
 #include "LinkBinTree.h"
-Status InitLBT(BitNodePtr &T){}
-Status CreLBT(BitNodePtr &T){}
+Status InitLBT(BitNodePtr &T){
+    T = (BitNodePtr)malloc(sizeof(BitNode));
+    T->lchild = NULL;
+    T->rchild = NULL;
+}
+Status CreLBT(BitNodePtr &T){
+    cout << "please input the node value:";
+    char n;
+    cin >> n;
+    BitNodePtr P;
+    P = T;
+    while(n != ' '){
+        InitLBT(P);
+        P->data = n;
+
+    }
+
+}
+Status PreCreLBT(BitNodePtr T){//T need Init
+    char n;
+    cin >> n;
+    if(n != ' '){
+        T->data = n;
+        InitLBT(T->lchild);
+        T = T->lchild;
+        PreTraLBT(T);
+        InitLBT(T->rchild);
+        T = T->rchild;
+        PreTraLBT(T);
+    }
+    free(T);
+}
 Status DesLBT(BitNodePtr &T){}
 Status ClearLBT(BitNodePtr &T){}
 Status DepthLBT(BitNodePtr T){}
@@ -16,8 +46,29 @@ BitNodePtr LeftBroLBT(BitNodePtr T, BitNodePtr E){}
 BitNodePtr RightBroLBT(BitNodePtr T, BitNodePtr E){}
 Status InsChild(BitNodePtr T, BitNodePtr P, int LR, BitNodePtr C){}
 Status DelChild(BitNodePtr T, int LR, BitNodePtr P){}
-Status PreTraLBT(BitNodePtr T){}
-Status InTraLBT(BitNodePtr T){}
-Status PostTraLBT(BitNodePtr T){}
+Status PreTraLBT(BitNodePtr T){
+    Visit(T);
+    if(T->lchild != NULL)
+        PreTraLBT(T->lchild);
+    if(T->rchild != NULL)
+        PreTraLBT(T->rchild);
+}
+Status InTraLBT(BitNodePtr T){
+    if(T->lchild != NULL)
+        InTraLBT(T->lchild);
+    Visit(T);
+    if(T->rchild != NULL)
+        InTraLBT(T->rchild);
+}
+
+Status PostTraLBT(BitNodePtr T){
+    if(T->lchild != NULL)
+        PostTraLBT(T->lchild);
+    if(T->rchild != NULL)
+        PostTraLBT(T->rchild);
+    Visit(T);
+}
 Status LevelTraLeT(BitNodePtr T){}
-Status Visit(BitNodePtr T){}
+Status Visit(BitNodePtr T){
+    cout << T->data;
+}
